357394969efde1a8b41ced434a6fd56cc67ecc5d
Andrea C. Crawford <acm@us.ibm.com>
2025-08-12 10:37:34 -0400

    updates to test case examples

--- test_correctness.py
+++ test_correctness.py
@@ -1,21 +1,22 @@
-import pytest
-from deepeval import assert_test
+from deepeval import evaluate
 from deepeval.metrics import GEval
 from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 
-def test_case():
-    correctness_metric = GEval(
-        name="Correctness",
-        criteria="Determine if the 'actual output' is correct based on the 'expected output'.",
-        evaluation_params=[LLMTestCaseParams.ACTUAL_OUTPUT, LLMTestCaseParams.EXPECTED_OUTPUT],
-        threshold=0.5
-    )
-    test_case = LLMTestCase(
+correctness_metric = GEval(
+    name="Correctness",
+    criteria="Determine if the 'actual output' is correct based on the 'expected output'.",
+    evaluation_params=[LLMTestCaseParams.ACTUAL_OUTPUT, LLMTestCaseParams.EXPECTED_OUTPUT],
+    threshold=0.5
+)
+
+test_cases = [
+    LLMTestCase(
         input="What is the capital of France?",
         # Replace this with the actual output from your LLM application
         actual_output="The capital of France is Paris.",
         expected_output="Paris is the capital of France. Paris is located along the Seine River.",
         retrieval_context=["France is a country in Western Europe."]
     )
+]
 
-    assert_test(test_case, [correctness_metric])
+evaluate(test_cases, [correctness_metric])
--- test_hallucinations.py
+++ test_hallucinations.py
@@ -8,10 +8,14 @@ test_cases = [
     LLMTestCase(
         input="What is the capital of France?",
         context=["France is a country in Western Europe."],
-        #role="user",
-        #content="What is the capital of France?",
-        actual_output="Paris is the capital of France.",
-        expected_output="Paris is the capital of France. France is a country in Western Europe that is located on the Seine River.",
+        actual_output="The capital of France is Paris.",
+        expected_output="Paris is the capital of France. Paris is located along the Seine River.",
+    ),
+    LLMTestCase(
+        input="How old is Ozzy Osbourne?",
+        context=["Ozzy Osbourne is a British born singer and songwriter. He is most notable for the leading the band Black Sabbath and his solo singing career."],
+        actual_output="Ozzy Osbourne, born John Michael Osbourne on December 3, 1948, is 73 years old, as of the knowledge cutoff in April 2024. He is an English singer",
+        expected_output="Ozzy Osbourne passed away on July 22, 2025 (age 76 years)",
     )
 ]
 
